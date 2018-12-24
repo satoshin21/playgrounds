6e490d41680ac2fa0641608854b0c60a4fbcd483
Satoshi Nagasaka <satoshi.nagasaka21@gmail.com>
2018-12-24 16:37:02 +0900

    font playground

--- Font.playground/Contents.swift
+++ Font.playground/Contents.swift
@@ -1,10 +1,13 @@
 //: Playground - noun: a place where people can play
 
+import Foundation
 import UIKit
+import PlaygroundSupport
 
 var str = "Hello, playground"
 
 let font = UIFont(name: "HiraKakuProN-W3", size: 12)!
+//let font = UIFont.systemFont(ofSize: 12)
 //let font = UIFont.systemFont(ofSize: 12)
 font.fontName   // TimesNewRomanPSMT
 font.familyName // Times New Roman
@@ -16,4 +19,54 @@ font.ascender
 font.descender
 font.leading
 font.lineHeight
-let lineHeight = abs(font.descender) + font.ascender
+let trueLineHeight =
+    abs(font.descender)
+        + font.ascender
+        + font.leading
+
+let hogehoge = "hogehoge"
+
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 640, height: 300))
+view.backgroundColor = .gray
+let padding: CGFloat = 20
+
+let textViewFrame = CGRect(
+    x: padding,
+    y: padding,
+    width: view.frame.width - padding*2,
+    height: view.frame.height - padding*2)
+let textView = UITextView(
+    frame: textViewFrame)
+view.addSubview(textView)
+
+let paragraphStyle = NSMutableParagraphStyle()
+paragraphStyle.firstLineHeadIndent = 50
+paragraphStyle.headIndent = 10
+
+textView.attributedText = NSAttributedString(string: "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。", attributes: [.paragraphStyle: paragraphStyle])
+//textView.text = "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。"
+textView.textContainer
+textView.textContainer.lineFragmentPadding = 20
+textView.textContainerInset
+textView.textContainerInset = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 10)
+
+textView.font = UIFont.systemFont(ofSize: 40)
+textView.textContainer.lineBreakMode = .byWordWrapping
+textView.layoutManager.allowsNonContiguousLayout = true
+
+textView.layoutManager.hasNonContiguousLayout
+
+
+
+textView.backgroundColor = .white
+//
+//let textView2 = UITextView(frame: textViewFrame)
+//view.addSubview(textView2)
+//textView2.text = "foo"
+////textView.textStorage.addLayoutManager(textView2.layoutManager)
+//textView.text = "bar"
+//
+//textView2.text = "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。"
+
+
+PlaygroundPage.current.liveView = view
